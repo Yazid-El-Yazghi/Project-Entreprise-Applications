260b0c4ccf7332459895c4d285610993ed9ad01f
Yazid <yaya250@live.fr>
2025-05-29 20:13:28 +0200

    location controller

--- src/main/java/be/ehb/ngo/controller/LocationController.java
+++ src/main/java/be/ehb/ngo/controller/LocationController.java
@@ -1,4 +1,13 @@
 package be.ehb.ngo.controller;
 
+import be.ehb.ngo.model.Location;
+import be.ehb.ngo.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class LocationController {
+    @Autowired
+    private LocationRepository locationRepository;
 }
